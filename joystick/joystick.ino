fc00ecbc956863957afbf21589cbe922ee764a7c
Trammell Hudson <hudson@trmm.net>
2017-05-12 19:15:13 -0400

    joystick updates and a box to hold them

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -5,16 +5,21 @@
  * and two buttons.
  */
 
-#define JOY_U	0
-#define JOY_D	2
-#define JOY_L	1
-#define JOY_R	3
+#define JOY_U	1
+#define JOY_D	3
+#define JOY_L	2
+#define JOY_R	0
 
 #define BUTTON_0 4
 #define BUTTON_1 5
 
-#define JOY_LED	10
+#define BUTTON_0_LED 10
+#define BUTTON_1_LED 9
+#define JOY_LED	12
+
 unsigned int joy_pwm;
+unsigned int button_0_pwm;
+unsigned int button_1_pwm;
 
 void setup()
 {
@@ -28,7 +33,15 @@ void setup()
 	pinMode(JOY_LED, OUTPUT);
 	analogWrite(JOY_LED, 0);
 
+	pinMode(BUTTON_0_LED, OUTPUT);
+	analogWrite(BUTTON_0_LED, 0);
+
+	pinMode(BUTTON_1_LED, OUTPUT);
+	analogWrite(BUTTON_1_LED, 0);
+
 	Serial.begin(9600);
+
+	joy_pwm = button_0_pwm = button_1_pwm = 0;
 }
 
 void loop()
@@ -37,14 +50,30 @@ void loop()
 	int d = !digitalRead(JOY_D);
 	int l = !digitalRead(JOY_L);
 	int r = !digitalRead(JOY_R);
-	int button0 = !digitalRead(BUTTON_0);
-	int button1 = !digitalRead(BUTTON_1);
+	int button_0 = !digitalRead(BUTTON_0);
+	int button_1 = !digitalRead(BUTTON_1);
 
 	if (joy_pwm >= 256)
 		analogWrite(JOY_LED, 512 - joy_pwm);
 	else
 		analogWrite(JOY_LED, joy_pwm);
 	joy_pwm = (joy_pwm + 1) % 512;
+
+	if (button_0)
+		button_0_pwm = 0;
+	if (button_0_pwm != 255)
+	{
+		button_0_pwm++;
+		analogWrite(BUTTON_0_LED, button_0_pwm);
+	}
+
+	if (button_1)
+		button_1_pwm = 0;
+	if (button_1_pwm != 255)
+	{
+		button_1_pwm++;
+		analogWrite(BUTTON_1_LED, button_1_pwm);
+	}
 
 	if (u)
 		Keyboard.set_key1(KEY_UP);
@@ -62,17 +91,20 @@ void loop()
 	else
 		Keyboard.set_key2(0);
 
-	Keyboard.set_key3(button0 ? KEY_SPACE : 0);
-	Keyboard.set_key4(button1 ? KEY_Z : 0);
+	Keyboard.set_key3(button_0 ? KEY_SPACE : 0);
+	Keyboard.set_key4(button_1 ? KEY_Z : 0);
 	Keyboard.send_now();
 
-	Serial.print(u ? " " : "U");
-	Serial.print(d ? " " : "D");
-	Serial.print(l ? " " : "L");
-	Serial.print(r ? " " : "R");
-	Serial.print(button0 ? " " : "0");
-	Serial.print(button1 ? " " : "1");
+	if (0)
+	{
+	Serial.print(!u ? " " : "U");
+	Serial.print(!d ? " " : "D");
+	Serial.print(!l ? " " : "L");
+	Serial.print(!r ? " " : "R");
+	Serial.print(!button_0 ? " " : "0");
+	Serial.print(!button_1 ? " " : "1");
 	Serial.println();
+	}
 
-	delay(10);
+	//delay(10);
 }
